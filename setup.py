cec8ea1539fada28aa6f1beb6020df0377f117fa
Jeevant010 <jeevantmudgil10@gmail.com>
2025-06-22 22:20:30 +0530

    A lot of changes

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+with open("README.md" , "r" , encoding="utf-8") as f:
+    long_description = f.read()
+    
+__version__ = "0.0.0"
+
+REPO_NAME = "AI_Summarizer"
+AUTHER_USER_NAME = "jeevant"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "jeevantmudgil10@gmail.com"
